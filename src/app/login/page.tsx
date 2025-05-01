faf73aa22d263f505ffaf6ca959c688e2d4e44f3
mrzzrm <mrzzrm27@gmail.com>
2025-05-01 15:14:18 +0330

    login page view complete

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,7 @@
 
 import {useFormState} from 'react-dom';
 import {sendOtpAction} from './actions';
+import {Button} from "@/components/ui/button";
 
 export default function LoginPage() {
     const [state, action] = useFormState(sendOtpAction, null);
@@ -28,7 +29,7 @@ export default function LoginPage() {
                         <img
                             alt="Picture of the author" loading="lazy" width="56" height="56" decoding="async"
                             data-nimg="1"
-                            className="logo m-auto mb-6 mt-4 w-[180px]" src="/images/login-logo.svg"
+                            className="logo m-auto mb-6 mt-4 w-[180px]" src="/images/fake-logo.png"
                         />
                     </a>
                     <section className="flex w-[310px]  overflow-hidden">
@@ -37,28 +38,25 @@ export default function LoginPage() {
                         >
                             <section
                                 className="h-[325px] w-[300px] text-center transition-all duration-700 ease-in-out"><h2
-                                className="mb-[20px] text-[30px] font-semibold text-primary text-center">خوش آمدید!</h2>
-                                <section className="mb-[9px] flex gap-1 justify-center"><span
-                                    className="title ">ورود</span><span>|</span> <span>ثبت نام</span></section>
+                                className="mb-[20px] text-[30px] font-semibold text-brand text-center">خوش آمدید!</h2>
+                                <section className="mb-[9px] flex gap-1 justify-center text-lg"><span
+                                    className=" ">ورود</span><span>|</span> <span>ثبت نام</span></section>
                                 <form
                                     className="mx-auto mb-8 flex w-[90%] max-w-full flex-col items-start md:w-full xl:mb-[181px] xl:w-[280px]">
-                                    <h2 className="title w-full mb-[35px] text-center">لطفا شماره موبایل خود را وارد
+                                    <h2 className="text-xl w-full mb-[35px] text-center">لطفا شماره موبایل خود را وارد
                                         کنید</h2>
                                     <section className="relative w-full">
                                         <input
                                             placeholder="شماره موبایل خود را وارد کنید"
                                             className=" false mx-0 h-[48px] w-full rounded-[4px] border-[1px] border-[#C2C2C2] p-[15px] text-xs outline-none transition-all duration-300 focus:border-[#0165e1] undefined"/>
                                     </section>
-                                    <button type="submit"
-                                            className="relative flex h-12 false items-center justify-center rounded-[10px] bg-primary text-black px-5 transition-all duration-700 disabled:opacity-50 w-full mt-7 hover:shadow-2xl focus:shadow-none">تایید
+                                    <Button type="submit" variant={"default"}
+                                            className="relative flex h-12  items-center justify-center rounded-md bg-brand  px-5 transition-all duration-700 disabled:opacity-50 w-full mt-7 hover:shadow-2xl focus:shadow-none">تایید
                                         و دریافت کد
-                                        <div className="btnLoader  opacity-0">
-                                            <div></div>
-                                            <div></div>
-                                            <div></div>
-                                            <div></div>
+                                        <div className="btnLoader">
+
                                         </div>
-                                    </button>
+                                    </Button>
                                 </form>
                             </section>
                         </section>
