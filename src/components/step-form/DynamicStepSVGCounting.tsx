7a4b24a57f6b11021e9cbeacc689f81a3c77164e
mrzzrm <mrzzrm27@gmail.com>
2025-05-12 17:19:13 +0330

    wizard logic issue

--- src/components/step-form/DynamicStepSVGCounting.tsx
+++ src/components/step-form/DynamicStepSVGCounting.tsx
@@ -18,10 +18,7 @@ export const DynamicStepSVGCounting = ({
     const circleRefs = useRef<(SVGCircleElement | null)[]>([]);
     const pathRefs = useRef<(SVGPathElement | null)[]>([]);
 
-    const isActive = (i: number) => {
-        const stepIndexFromRight = stepsConfig - 1 - i;
-        return stepIndexFromRight <= currentIndex;
-    };
+
     const reversedPathRefs = [...pathRefs.current].reverse();
 
     useEffect(() => {
@@ -75,8 +72,22 @@ export const DynamicStepSVGCounting = ({
         });
     }, [stepsConfig]);
 
+
+    const isPreCurrent = (i: number,currentIndex: number) => {
+        const stepIndexFromRight = stepsConfig - 1 - i;
+        return stepIndexFromRight < currentIndex;
+
+    };
+
+    function IsCurrentStep(i: number,currentIndex: number) {
+
+        return (stepsConfig - 1 - i) == currentIndex ;
+    }
+
     const renderCircles = () =>
         Array.from({length: stepsConfig}).map((_, i) => {
+
+            console.log({i,currentIndex,IC:IsCurrentStep(i,currentIndex),PC:isPreCurrent(i ,currentIndex)})
             const cx = 10 + (circleRadius + i * gapBetweenSteps);
             return (
                 <g key={i}>
@@ -84,29 +95,29 @@ export const DynamicStepSVGCounting = ({
                         ref={(el) => (circleRefs.current[i] = el)}
                         cx={cx}
                         cy="30"
-                        r={(i - 1 !== currentIndex) ? circleRadius : circleRadius - 2}
-                        fill={isActive(i) ? (i - 1 !== currentIndex) ? "#28a745" : "#6E21FF" : "#98CEFF"}
+                        r={IsCurrentStep(i,currentIndex) ? circleRadius : circleRadius - 2}
+                        fill={IsCurrentStep(i ,currentIndex) ? "#6E21FF" : isPreCurrent(i ,currentIndex)?"#28a745":"#98CEFF"  }
                     />
                     <text
                         x={cx}
                         y="10"
                         fontSize="12"
                         textAnchor="middle"
-                        fill={isActive(i) ? (i - 1 !== currentIndex)?"#28a745":"#6E21FF" : "#98CEFF"}
+                        fill={IsCurrentStep(i ,currentIndex) ? "#6E21FF" : isPreCurrent(i ,currentIndex)?"#28a745":"#98CEFF"}
                     >
                         {stepTexts[i]}
                     </text>
-                    {(isActive(i)) && (
+
                         <>
                             <circle
                                 cx={cx}
                                 cy="30"
                                 r="13.5"
-                                stroke={(i - 1 !== currentIndex)?"#28a745":"#6E21FF"}
+                                stroke={IsCurrentStep(i ,currentIndex) ? "#6E21FF" : isPreCurrent(i ,currentIndex)?"#28a745":"#98CEFF"}
                                 strokeWidth="3"
                             />
 
-                            {(i - 1 !== currentIndex) && <text
+                            {isPreCurrent(i,currentIndex)&& <text
                                 x={cx}
                                 y="34"
                                 fontSize="16"
@@ -116,16 +127,16 @@ export const DynamicStepSVGCounting = ({
                                 ✓
                             </text>}
                         </>
-                    )}
+
                 </g>
             );
         });
 
     const renderPaths = () =>
         Array.from({length: stepsConfig - 1}).map((_, i) => {
-            const x1 = 15 + circleRadius + i * gapBetweenSteps + 15;
-            const x2 = 10 + circleRadius + (i + 1) * gapBetweenSteps - 10;
-            const activePath = isActive(i) && isActive(i + 1);
+            const x1 = 2 + circleRadius + i * gapBetweenSteps + 2;
+            const x2 = 2 + circleRadius + (i + 1) * gapBetweenSteps - 2;
+            const activePath = isPreCurrent(i) && isPreCurrent(i + 1);
 
             return (
                 <path
--- src/components/step-form/OnboardingFlowForm1.tsx
+++ src/components/step-form/OnboardingFlowForm1.tsx
@@ -1,11 +1,13 @@
 "use client"
 import React, {useState} from 'react';
-import FreeTowingInfo from "@/components/step-form/Free-Towing-Info";
 import BimeForm from "@/components/step-form/BimeForm";
 import {DynamicStepSVGCounting} from "@/components/step-form/DynamicStepSVGCounting";
+import {Button} from "@/components/ui/button";
+import {CheckCheck, FileCheck2, TicketCheck} from "lucide-react";
+import FreeTowingInfo from "@/components/step-form/Free-Towing-Info";
 
 
-const StepThree = ({goToNext,goToPrev}) => (
+const StepThree = ({goToNext, goToPrev}) => (
     <>
         <h1>Step 3</h1>
         <button onClick={() => goToNext({age: 50})}>Next</button>
@@ -13,13 +15,145 @@ const StepThree = ({goToNext,goToPrev}) => (
     </>
 );
 
-const StepFour = ({goToNext,goToPrev}) => (
+const StepFour = ({goToNext, goToPrev}) => (
     <>
         <h1>Step 4</h1>
         <button onClick={() => goToNext({hairColor: 'brown'})}>Next</button>
         <button onClick={() => goToPrev({hairColor: 'brown'})}>prev</button>
     </>
 );
+
+function PrePayment() {
+    return <div className="max-w-[80%] mx-auto">
+        <div className="bg-red-100 p-4 rounded-md mb-4">
+            <p className="text-red-600 text-lg font-semibold">توجه:</p>
+            <p className="text-red-600">قبل از زدن دکمه پرداخت و ورود به درگاه پرداخت حتما VPN خود را خاموش
+                کنید</p>
+        </div>
+
+        <div className="bg-white p-6 rounded-lg shadow-md">
+            <div className="space-y-4">
+                <div className="flex justify-between">
+                    <span className="font-semibold text-gray-700">نوع سرویس:</span>
+                    <span className="text-gray-700">بیمه امداد حمل رایگان</span>
+                </div>
+
+                <div className="flex justify-between">
+                    <span className="font-semibold text-gray-700">سقف تعهد:</span>
+                    <span className="text-gray-700">۵,۰۰۰,۰۰۰ تومان</span>
+                </div>
+
+                <div className="flex justify-between">
+                    <span className="font-semibold text-gray-700">قیمت بیمه نامه:</span>
+                    <span className="text-gray-700">۳۵۰,۰۰۰ تومان</span>
+                </div>
+
+                <div className="flex justify-between">
+                    <span className="font-semibold text-gray-700">مالیات بر ارزش افزوده:</span>
+                    <span className="text-gray-700">۳۵,۰۰۰ تومان</span>
+                </div>
+            </div>
+
+            <div className="mt-4 flex justify-between">
+                <span className="font-semibold text-lg">مبلغ قابل پرداخت:</span>
+                <span className="text-lg text-gray-800">۳۷۵,۰۰۰ تومان</span>
+            </div>
+
+            <div className="mt-6 flex gap-4 justify-center">
+
+                <Button
+                    variant={"outline"}
+                    className="">
+                    مرحله قبل
+                </Button>
+                <Button
+                    className="min-w-[160px]"
+                >
+                    پرداخت
+                </Button>
+            </div>
+
+
+        </div>
+    </div>;
+}
+
+function PaymentSuccessful() {
+    return <div className="max-w-[80%] mx-auto">
+        <div className="bg-green-100 p-6 rounded-lg shadow-md mb-4">
+            <div className="flex items-center flex-col space-x-3">
+    <span className="text-green-600 text-2xl">
+
+        <TicketCheck size="50" className=""/>
+    </span>
+                <span className="text-green-600 font-semibold">پرداخت با موفقیت انجام شد</span>
+            </div>
+        </div>
+
+        <div className="bg-white p-6 rounded-lg shadow-md">
+            <div className="space-y-4">
+                <div className="flex justify-between">
+                    <span className="font-semibold text-gray-700">به پرداخت ملت</span>
+                    <span className="text-gray-700">دریافت کننده</span>
+                </div>
+
+                <div className="flex justify-between">
+                    <span className="font-semibold text-gray-700">تاریخ و ساعت:</span>
+                    <span className="text-gray-700">۱۴۰۴/۰۵/۰۷ - ۱۱:۴۵</span>
+                </div>
+
+                <div className="flex justify-between">
+                    <span className="font-semibold text-gray-700">مبلغ:</span>
+                    <span className="text-gray-700">۲,۷۵۰,۰۰۰ ریال</span>
+                </div>
+
+                <div className="flex justify-between">
+                    <span className="font-semibold text-gray-700">کدپیگیری:</span>
+                    <span className="text-gray-700">BCD69B9BE93B</span>
+                </div>
+
+                <div className="flex justify-between mt-4">
+                    <span className="font-semibold text-gray-700">وضعیت:</span>
+                    <span className="text-green-600">پرداخت شده</span>
+                </div>
+            </div>
+        </div>
+        <div className="mt-6 flex gap-4 justify-center">
+
+
+            <Button
+                className="min-w-[260px]"
+            >
+                بازگشت به سایت
+            </Button>
+        </div>
+    </div>;
+}
+
+function PrintAndDownloadBime() {
+    return <div className="max-w-[80%] mx-auto">
+        <div className="bg-green-100 p-6 rounded-lg shadow-md mb-4">
+            <div className="flex  flex-col items-center space-x-3">
+    <span className="text-green-600 text-2xl  ">
+<FileCheck2 size={50}/>
+    </span>
+                <span className="text-green-600 text-xl font-semibold">اطلاعات شما با موفقیت ثبت شد</span>
+            </div>
+
+        </div>
+        <p className="text-gray-700 text-center  mt-2">کاربر گرامی از طریق دکمه زیر می‌توانید اقدام به دریافت بیمه
+            نامه خود نمایید.</p>
+        <div className="bg-white p-6 rounded-lg shadow-md mt-4 flex flex-col justify-center">
+            <Button
+                className="w-full max-w-3xl mx-auto ">
+                دریافت بیمه نامه
+            </Button>
+            <div className="mt-4 text-center">
+                <a href="#" className="text-sm text-gray-600 hover:underline">بازگشت به صفحه اصلی</a>
+            </div>
+        </div>
+    </div>;
+}
 
 function OnboardingFlow() {
     const [onboardingData, setOnboardingData] = useState({});
@@ -56,6 +190,16 @@ function OnboardingFlow() {
 
                 <BimeForm/>
 
+
+                <>
+                    <PrintAndDownloadBime/>
+
+
+                    <PaymentSuccessful/>
+
+
+                    <PrePayment/>
+                </>
                 <StepThree/>
 
                 <StepFour/>
