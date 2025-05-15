ad957e241652211e07544a428de9590f6eec01b7
mrzzrm <mrzzrm27@gmail.com>
2025-05-15 19:58:30 +0330

    replace radio instead of button

--- src/components/step-form/BimeForm.tsx
+++ src/components/step-form/BimeForm.tsx
@@ -1,47 +1,44 @@
 import { useState } from "react";
 import MarkaziBimeRegister from "@/components/step-form/MarkaziBimeRegister";
 import ManualBimeRegister from "@/components/step-form/ManualBimeRegister2";
-import {Button} from "@/components/ui/button";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { Label } from "@/components/ui/label";
 
 export default function BimeForm({ goToNext, goToPrev, onboardingData }) {
-    const [mode, setMode] = useState<"markazi" | "manual">("markazi");
-
+    const [mode, setMode] = useState<"markazi" | "manual">("manual");
     const sharedProps = { goToNext, goToPrev, onboardingData };
 
     return (
-        <div className="max-w-7xl mx-auto">
-            <section className="text-red-600 text-center bg-rose-100 p-2 mt-4 leading-normal rounded-md text-[18px] py-4 m-2"><p>نکته: در صورتی
-                که ثبت نام به کمک استعلام بیمه مرکزی با مشکل مواجه شد می توانید از ثبت نام دستی استفاده کنید.</p><p
-                className="mt-4">نکته: در صورتی که دارای پلاک مناطق آزاد هستید از ثبت نام دستی استفاده کنید.</p>
-            </section>
-            <section className="flex m-4">
-                <section className="grid w-full grid-cols-1 gap-2 xl:grid-cols-2 mt-5">
-                    <Button
-                        type="button"
-                        onClick={() => setMode("markazi")}
-                        className={`p-3 font-bold text-center rounded-md transition-all cursor-pointer hover:text-white ${
-                            mode === "markazi"
-                                ? "bg-brand scale-105 text-white"
-                                : "bg-gray-200 text-black "
-                        }`}
-                    >
-                        ثبت نام به کمک استعلام بیمه مرکزی
-                    </Button>
-                    <Button
-                        type="button"
-                        onClick={() => setMode("manual")}
-                        className={`p-3 font-bold text-center rounded-md transition-all cursor-pointer  hover:text-white ${
-                            mode === "manual"
-                                ? "bg-brand scale-105 text-white"
-                                : "bg-gray-200 text-black "
-                        }`}
-                    >
-                        ثبت نام دستی
-                    </Button>
-                </section>
+        <div className="max-w-7xl mx-auto px-4" >
+            {/* هشدار */}
+            <section className="bg-rose-100 text-red-600 text-center p-4 mt-4 rounded-md text-[18px] leading-relaxed space-y-4">
+                <p>
+                    نکته: در صورتی که ثبت‌نام به کمک استعلام بیمه مرکزی با مشکل مواجه شد، می‌توانید از ثبت‌نام دستی استفاده کنید.
+                </p>
+                <p>
+                    نکته: در صورتی که دارای پلاک مناطق آزاد هستید، از ثبت‌نام دستی استفاده کنید.
+                </p>
             </section>
 
-            <div className="max-w-7xl m-auto mt-5">
+            <section className="mt-6 ">
+                <RadioGroup
+                    value={mode}
+                    dir="rtl"
+                    onValueChange={(value) => setMode(value as "markazi" | "manual")}
+                    className=""
+                >
+                    <div className="flex items-center space-x-2 p-2 cursor-pointer">
+                        <RadioGroupItem className="ml-2" value="markazi" id="r1" />
+                        <Label  htmlFor="r1">ثبت‌نام به کمک استعلام بیمه مرکزی</Label>
+                    </div>
+                    <div className="flex items-center space-x-2 p-2 cursor-pointer">
+                        <RadioGroupItem className="ml-2" value="manual" id="r2" />
+                        <Label  htmlFor="r2">ثبت‌نام دستی</Label>
+                    </div>
+                </RadioGroup>
+            </section>
+
+            <div className="mt-8">
                 {mode === "markazi" ? (
                     <MarkaziBimeRegister {...sharedProps} />
                 ) : (
@@ -51,4 +48,3 @@ export default function BimeForm({ goToNext, goToPrev, onboardingData }) {
         </div>
     );
 }
-
